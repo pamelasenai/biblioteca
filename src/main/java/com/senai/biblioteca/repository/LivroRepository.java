4bede265b2b14bce03299ba9723340fd7f08ccef
Pâmela Vieira da Silva <57575373+panhavsilva@users.noreply.github.com>
2024-03-14 16:30:00 -0300

    :sparkles: adiciona repository de livro

---
+++ src/main/java/com/senai/biblioteca/repository/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.senai.biblioteca.repository;
+
+import com.senai.biblioteca.entities.LivroEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<LivroEntity, Integer> {
+}
