79431672a9be96d02e25d1ef539f253f5f0dfe42
Pâmela Vieira da Silva <57575373+panhavsilva@users.noreply.github.com>
2024-03-14 16:18:52 -0300

    :sparkles: adiciona entidade emprestimo

---
+++ src/main/java/com/senai/biblioteca/entities/EmprestimoEntity.java
@@ -0,0 +1,27 @@
+package com.senai.biblioteca.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "emprestimos")
+public class EmprestimoEntity {
+    @Id
+    private Integer id;
+
+    @Column(name = "data_emprestimo")
+    private LocalDate dataEmprestimo;
+    @Column(name = "data_devolucao")
+    private LocalDate dataDevolucao;
+
+    @ManyToOne
+    @JoinColumn(name = "id_livro", referencedColumnName = "id")
+    private LivroEntity idLivro;
+
+    @ManyToOne
+    @JoinColumn(name = "id_membro", referencedColumnName = "id")
+    private MembroEntity idMembro;
+}
