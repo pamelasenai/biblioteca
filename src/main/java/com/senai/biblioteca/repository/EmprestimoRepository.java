84ff5dfd5f0046c77b623fafb2d4ebf3cc312179
Pâmela Vieira da Silva <57575373+panhavsilva@users.noreply.github.com>
2024-03-16 14:48:02 -0300

    :sparkles: atualiza repository

--- src/main/java/com/senai/biblioteca/repository/EmprestimoRepository.java
+++ src/main/java/com/senai/biblioteca/repository/EmprestimoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmprestimoRepository extends JpaRepository<EmprestimoEntity, Integer> {
+public interface EmprestimoRepository extends JpaRepository<EmprestimoEntity, Long> {
 }
--- src/main/java/com/senai/biblioteca/repository/VisitanteRepository.java
+++ src/main/java/com/senai/biblioteca/repository/VisitanteRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VisitanteRepository extends JpaRepository<VisitanteEntity, Integer> {
+public interface VisitanteRepository extends JpaRepository<VisitanteEntity, Long> {
 }
