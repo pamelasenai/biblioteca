0b00ea1bbbc04679014ae953be6d4021f1fac2a0
panha <pamela_vs@estudante.sc.senai.br>
2024-03-17 19:04:05 -0300

    :sparkles: adiciona query em visitante

--- src/main/java/com/senai/biblioteca/repository/VisitanteRepository.java
+++ src/main/java/com/senai/biblioteca/repository/VisitanteRepository.java
@@ -2,8 +2,19 @@ package com.senai.biblioteca.repository;
 
 import com.senai.biblioteca.entities.VisitanteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VisitanteRepository extends JpaRepository<VisitanteEntity, Long> {
+    @Query(
+            "update VisitanteEntity visitante set visitante.nome = :nome" +
+            " visitante.telefone = :telefone" +
+            " where visitante.id = :id"
+    )
+    VisitanteEntity update(
+            @Param("nome") String nome,
+            @Param("telefone") String telefone
+    );
 }
