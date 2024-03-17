eabdb12fd26e6f8bb7b4331b673e2fa0f4004e91
panha <pamela_vs@estudante.sc.senai.br>
2024-03-17 19:03:27 -0300

    :sparkles: adiciona query em emprestimo

--- src/main/java/com/senai/biblioteca/repository/EmprestimoRepository.java
+++ src/main/java/com/senai/biblioteca/repository/EmprestimoRepository.java
@@ -2,8 +2,25 @@ package com.senai.biblioteca.repository;
 
 import com.senai.biblioteca.entities.EmprestimoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmprestimoRepository extends JpaRepository<EmprestimoEntity, Long> {
+    @Query(
+            "update EmprestimoEntity emprestimo set emprestimo.dataEmprestimo = :dataEmprestimo" +
+            " emprestimo.dataDevolucao = :dataDevolucao " +
+            " emprestimo.livro = :livro" +
+            " emprestimo.membro = :membro" +
+            " emprestimo.bibliotecario = :bibliotecario" +
+            " where emprestimo.id = :id"
+    )
+    EmprestimoEntity update(
+            @Param("dataEmprestimo") String dataEmprestimo,
+            @Param("dataDevolucao") String dataDevolucao,
+            @Param("livro") Long livro,
+            @Param("membro") Long membro,
+            @Param("bibliotecario") Long bibliotecario
+    );
 }
