ef45bca408a2c33478687279b10331d7a7eee66f
panha <pamela_vs@estudante.sc.senai.br>
2024-03-17 17:10:31 -0300

    :sparkles: adiciona busca de livro

--- src/main/java/com/senai/biblioteca/controller/LivroController.java
+++ src/main/java/com/senai/biblioteca/controller/LivroController.java
@@ -3,10 +3,10 @@ package com.senai.biblioteca.controller;
 import com.senai.biblioteca.entities.LivroEntity;
 import com.senai.biblioteca.service.LivroService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/livros")
@@ -15,7 +15,17 @@ public class LivroController {
     private final LivroService livroService;
 
     @PostMapping
-    private LivroEntity criarLivro(@RequestBody LivroEntity livro) throws Exception {
+    public LivroEntity criarLivro(@RequestBody LivroEntity livro) throws Exception {
         return livroService.salvarLivro(livro);
+    }
+
+    @GetMapping
+    public List<LivroEntity> buscarTodosLivros() {
+        return livroService.buscarTodosLivros();
+    }
+
+    @GetMapping("{id}")
+    public Optional<LivroEntity> buscarPorId(@PathVariable Long id) {
+        return livroService.buscarPorId(id);
     }
 }
