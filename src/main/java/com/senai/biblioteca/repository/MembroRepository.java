5c43e9cd3e2d4e5d66e86aefa9b4d4d19abf1708
panha <pamela_vs@estudante.sc.senai.br>
2024-03-17 18:19:59 -0300

    :sparkles: adiciona delete a membro

--- src/main/java/com/senai/biblioteca/repository/MembroRepository.java
+++ src/main/java/com/senai/biblioteca/repository/MembroRepository.java
@@ -2,8 +2,11 @@ package com.senai.biblioteca.repository;
 
 import com.senai.biblioteca.entities.MembroEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MembroRepository extends JpaRepository<MembroEntity, Long> {
+    @Query(value = "SELECT COUNT(*) FROM emprestimos WHERE id_membro = :id", nativeQuery = true)
+    int countVinculos(Long id);
 }
