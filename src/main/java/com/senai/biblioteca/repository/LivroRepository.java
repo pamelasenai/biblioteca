8bdc4474c74273869108fe7037a608e4e1af094a
panha <pamela_vs@estudante.sc.senai.br>
2024-03-17 19:03:36 -0300

    :sparkles: adiciona query em livro

--- src/main/java/com/senai/biblioteca/repository/LivroRepository.java
+++ src/main/java/com/senai/biblioteca/repository/LivroRepository.java
@@ -3,10 +3,24 @@ package com.senai.biblioteca.repository;
 import com.senai.biblioteca.entities.LivroEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LivroRepository extends JpaRepository<LivroEntity, Long> {
     @Query(value = "SELECT COUNT(*) FROM emprestimos WHERE id_livro = :id", nativeQuery = true)
     int countVinculos(Long id);
+
+    @Query(
+            "update LivroRepository livro set livro.titulo = :titulo" +
+            " livro.autor = :autor " +
+            " livro.senha = :senha" +
+            " livro.anoPublicacao = :anoPublicacao" +
+            " where livro.id = :id"
+    )
+    LivroRepository update(
+            @Param("titulo") String titulo,
+            @Param("autor") String autor,
+            @Param("anoPublicacao") Integer anoPublicacao
+    );
 }
