c34910e7aeaab766a5cb4797238c8efdfb5f7218
panha <pamela_vs@estudante.sc.senai.br>
2024-03-18 00:49:03 -0300

    :sparkles: atualiza entidade bibliotecario

--- src/main/java/com/senai/biblioteca/entities/BibliotecarioEntity.java
+++ src/main/java/com/senai/biblioteca/entities/BibliotecarioEntity.java
@@ -2,12 +2,10 @@ package com.senai.biblioteca.entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Table(name = "bibliotecario")
-@NoArgsConstructor
 public class BibliotecarioEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,17 +14,4 @@ public class BibliotecarioEntity {
     private String nome;
     private String email;
     private String senha;
-
-    public BibliotecarioEntity(Long id, String nome, String email, String senha) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.senha = senha;
-    }
-
-    public BibliotecarioEntity(String nome, String email, String senha) {
-        this.nome = nome;
-        this.email = email;
-        this.senha = senha;
-    }
 }
