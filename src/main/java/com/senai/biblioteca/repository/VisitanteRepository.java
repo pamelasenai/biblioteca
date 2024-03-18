6130ea627aafe1e02292f60d1c0f5e9a9d69e5fe
panha <pamela_vs@estudante.sc.senai.br>
2024-03-17 23:39:39 -0300

    :sparkles: add query visitante

--- src/main/java/com/senai/biblioteca/repository/VisitanteRepository.java
+++ src/main/java/com/senai/biblioteca/repository/VisitanteRepository.java
@@ -2,18 +2,19 @@ package com.senai.biblioteca.repository;
 
 import com.senai.biblioteca.entities.VisitanteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface VisitanteRepository extends JpaRepository<VisitanteEntity, Long> {
-    @Query(
-            "update VisitanteEntity visitante set visitante.nome = :nome" +
-            " visitante.telefone = :telefone" +
-            " where visitante.id = :id"
-    )
-    VisitanteEntity update(
+    @Modifying
+    @Transactional
+    @Query("UPDATE VisitanteEntity v SET v.nome = :nome, v.telefone = :telefone WHERE v.id = :id")
+    void update(
+            @Param("id") Long id,
             @Param("nome") String nome,
             @Param("telefone") String telefone
     );
