2636f97d1737ea5915c08c8f935d16d718605cdc
panha <pamela_vs@estudante.sc.senai.br>
2024-03-17 21:58:28 -0300

    :sparkles: add query bibliotecario

--- src/main/java/com/senai/biblioteca/repository/BibliotecarioRepository.java
+++ src/main/java/com/senai/biblioteca/repository/BibliotecarioRepository.java
@@ -2,24 +2,24 @@ package com.senai.biblioteca.repository;
 
 import com.senai.biblioteca.entities.BibliotecarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface BibliotecarioRepository extends JpaRepository<BibliotecarioEntity, Long> {
     @Query(value = "SELECT COUNT(*) FROM emprestimos WHERE id_bibliotecario = :id", nativeQuery = true)
     int countVinculos(Long id);
 
-    @Query(
-            "update BibliotecarioEntity bibliotecario set bibliotecario.nome = :nome" +
-            " bibliotecario.email = :email " +
-            " bibliotecario.senha = :senha" +
-            " where bibliotecario.id = :id"
-    )
-    BibliotecarioEntity update(
-        @Param("nome") String nome,
-        @Param("email") String email,
-        @Param("senha") String senha
+    @Modifying
+    @Transactional
+    @Query("update BibliotecarioEntity b SET b.nome = :nome, b.email = :email, b.senha = :senha where b.id = :id")
+    void update(
+            @Param("id") Long id,
+            @Param("nome") String nome,
+            @Param("email") String email,
+            @Param("senha") String senha
     );
 }
